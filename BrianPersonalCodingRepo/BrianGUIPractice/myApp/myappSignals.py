830cac82e7a919ab06bda684110c35c68cde0c73
sauerBT <briansauerborn@gmail.com>
2022-05-15 12:10:11 -0400

    Button was toggled with the checked state

--- BrianPersonalCodingRepo/BrianGUIPractice/myApp/myappSignals.py
+++ BrianPersonalCodingRepo/BrianGUIPractice/myApp/myappSignals.py
@@ -10,14 +10,22 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
+        self.button_is_checked = True
+
+        self.setWindowTitle("Signals")
 
         button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_toggled)
+        button.setChecked(self.button_is_checked)
 
         self.setMinimumSize(QSize(400,300))
 
         #Set central widget of the window
         self.setCentralWidget(button)
+
+    def the_button_was_toggled(self, checked):
+        self.button_is_checked = checked
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
