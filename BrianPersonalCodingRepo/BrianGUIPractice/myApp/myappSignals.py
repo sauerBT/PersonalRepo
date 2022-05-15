f8a149eaf2a56d86314533a6ed43b3607390f3b3
sauerBT <briansauerborn@gmail.com>
2022-05-15 12:49:56 -0400

    window title changed example that changes window title on button click and disabled the button when a specific title is set randomly

--- BrianPersonalCodingRepo/BrianGUIPractice/myApp/myappSignals.py
+++ BrianPersonalCodingRepo/BrianGUIPractice/myApp/myappSignals.py
@@ -4,18 +4,35 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
 
 # Only needed for access to command line arguments
 import sys
+from random import choice
+
+window_titles = [
+    'My App',
+    'My App',
+    'Still My App',
+    'Still My App',
+    'What on Earth',
+    'What on Earth',
+    'This is suprising',
+    'This is suprising',
+    'Something went wrong'
+]
 
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.n_times_clicked = 0
+
         self.button_is_checked = True
 
-        self.setWindowTitle("Signals")
+        self.setWindowTitle("My App")
 
         self.button = QPushButton("Press Me!")
         self.button.clicked.connect(self.the_button_was_clicked)
+
+        self.windowTitleChanged.connect(self.window_title_changed)
 
         self.setMinimumSize(QSize(400,300))
 
@@ -23,10 +40,16 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.button)
 
     def the_button_was_clicked(self):
-        self.button.setText("You already clicked me.")
-        self.button.setEnabled(False)
+        print("Clicked")
+        new_window_title = choice(window_titles)
+        print("Setting title: %s" % new_window_title)
+        self.setWindowTitle(new_window_title)
 
-        self.setWindowTitle("My One Shot App")
+    def window_title_changed(self, window_title):
+        print("Window title changed: %s" % window_title)
+
+        if window_title == 'Something went wrong':
+            self.button.setDisabled(True)
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
