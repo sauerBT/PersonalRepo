f6ca6f35cf1fead28afdb727734d350d73505d5a
sauerBT <briansauerborn@gmail.com>
2022-05-15 11:53:43 -0400

    Initial commit for signals chapter

---
+++ BrianPersonalCodingRepo/BrianGUIPractice/myApp/myappSignals.py
@@ -0,0 +1,36 @@
+from PyQt6 import QtWidgets
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
+
+# Only needed for access to command line arguments
+import sys
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press Me!")
+
+        self.setMinimumSize(QSize(400,300))
+
+        #Set central widget of the window
+        self.setCentralWidget(button)
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([])
+# works too.
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+#Start the event loop
+app.exec() 
+
+# Your application won't reach here until you exit and the event
+# loop has stopped.
+
