fba041829fda5606a9eea4cc278447e95dca6db0
sauerBT <briansauerborn@gmail.com>
2022-05-15 13:09:51 -0400

    Initial commit for the "Widgets" section examples

---
+++ BrianPersonalCodingRepo/BrianGUIPractice/myApp/07_Widgets.py
@@ -0,0 +1,46 @@
+from PyQt6 import QtWidgets
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLabel, QLineEdit, QVBoxLayout
+
+# Only needed for access to command line arguments
+import sys
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        self.label = QLabel()
+
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setMinimumSize(QSize(1000,800))
+
+        #Set central widget of the window
+        self.setCentralWidget(container)
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([])
+# works too.
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+#Start the event loop
+app.exec() 
+
+# Your application won't reach here until you exit and the event
+# loop has stopped.
+
