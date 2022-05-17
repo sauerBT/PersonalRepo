ffbf05d4db94f341aa8298d09f00216a0f7d651c
SauerBT <Briansauerborn@gmail.com>
2022-05-17 07:33:22 -0400

    Commit QSlider example

--- BrianPersonalCodingRepo/BrianGUIPractice/myApp/07_Widgets.py
+++ BrianPersonalCodingRepo/BrianGUIPractice/myApp/07_Widgets.py
@@ -1,6 +1,6 @@
 import re
 from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QSpinBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QSlider
 
 # Only needed for access to command line arguments
 import sys
@@ -12,18 +12,18 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
 
-        widget = QSpinBox()
-        # Or: widget = QDoubleSpinBox()
+        widget = QSlider()
 
         widget.setMinimum(-10)
         widget.setMaximum(3)
         # Or: widget.setRange(-10,3)
 
-        widget.setPrefix("$")
-        widget.setSuffix("c")
-        widget.setSingleStep(3) # Or e.g. 0.5 for QDoubleSpinBox
+        widget.setSingleStep(3)
         widget.valueChanged.connect(self.value_changed)
-        widget.textChanged.connect(self.value_changed_str)
+        widget.sliderMoved.connect(self.slider_position)
+        widget.sliderPressed.connect(self.slider_pressed)
+        widget.sliderReleased.connect(self.slider_released)
+
 
         self.setMinimumSize(QSize(300,200))
 
@@ -33,9 +33,14 @@ class MainWindow(QMainWindow):
     def value_changed(self, i): # i is an int
         print(i)
 
-    def value_changed_str(self, s): # s is a str
-        print(s)
+    def slider_position(self, p): 
+        print("position", p)
+        
+    def slider_pressed(self):
+        print("Pressed")
 
+    def slider_released(self):
+        print("Released")
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
 # If you know you won't use command line arguments QApplication([])
