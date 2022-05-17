93e907f2de7120b4e968f311f110cc151ce5be78
SauerBT <Briansauerborn@gmail.com>
2022-05-17 07:52:00 -0400

    Commit first layout example - widget background color

---
+++ BrianPersonalCodingRepo/BrianGUIPractice/myApp/08_Layouts/08_Layouts.py
@@ -0,0 +1,37 @@
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+from layout_colorwidget import Color
+
+# Only needed for access to command line arguments
+import sys
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        widget = Color('red')
+
+        self.setMinimumSize(QSize(300,200))
+
+        #Set central widget of the window
+        self.setCentralWidget(widget)
+        
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([])
+# works too.
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+#Start the event loop
+app.exec() 
+
+# Your application won't reach here until you exit and the event
+# loop has stopped.
+
