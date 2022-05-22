09ffe841c4ed324645e07c89347d5316053444ce
SauerBT <Briansauerborn@gmail.com>
2022-05-22 12:35:35 -0400

    Commit first example of section 9 - toolbar example

---
+++ BrianPersonalCodingRepo/BrianGUIPractice/myApp/08_Layouts/actionsToolbarsMenus.py
@@ -0,0 +1,48 @@
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import (QApplication, 
+QMainWindow,
+QLabel,
+QToolBar,
+QWidget)
+
+from layout_colorwidget import Color
+
+# Only needed for access to command line arguments
+import sys
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        label = QLabel("Hello!")
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.setMinimumSize(QSize(300,200))
+
+        #Set central widget of the window
+        self.setCentralWidget(label)
+
+        toolbar = QToolBar("My main toolbar")
+        self.addToolBar(toolbar)
+
+    def onMyToolBarButtonClick(self, s):
+        print("click", s)
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([])
+# works too.
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+#Start the event loop
+app.exec() 
+
+# Your application won't reach here until you exit and the event
+# loop has stopped.
+
