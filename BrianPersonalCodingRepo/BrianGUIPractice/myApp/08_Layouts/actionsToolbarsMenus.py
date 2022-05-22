644ff6ed1abc8ead2d075707abe94e6acab1a919
SauerBT <Briansauerborn@gmail.com>
2022-05-22 12:55:48 -0400

    Commit QAction .setCheckable example (makes the button toggleable)

--- BrianPersonalCodingRepo/BrianGUIPractice/myApp/08_Layouts/actionsToolbarsMenus.py
+++ BrianPersonalCodingRepo/BrianGUIPractice/myApp/08_Layouts/actionsToolbarsMenus.py
@@ -33,9 +33,10 @@ class MainWindow(QMainWindow):
         button_action = QAction("Your button", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.onMyToolBarButtonClick)
+        button_action.setCheckable(True) # <-- makes the Toolbar object toggleable
         toolbar.addAction(button_action)
 
-        self.setStatusBar(QStatusBar(self)) # <-- adds a status bar to the main window
+        self.setStatusBar(QStatusBar(self))
 
     def onMyToolBarButtonClick(self, s):
         print("click", s)
