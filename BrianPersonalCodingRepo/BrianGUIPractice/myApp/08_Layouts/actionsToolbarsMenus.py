7f8915875261d7450ca10662e6bc5cd04b05098a
SauerBT <Briansauerborn@gmail.com>
2022-05-23 08:14:09 -0400

    Commit first menubar example

--- BrianPersonalCodingRepo/BrianGUIPractice/myApp/08_Layouts/actionsToolbarsMenus.py
+++ BrianPersonalCodingRepo/BrianGUIPractice/myApp/08_Layouts/actionsToolbarsMenus.py
@@ -32,7 +32,7 @@ class MainWindow(QMainWindow):
         toolbar.setIconSize(QSize(16,16))
         self.addToolBar(toolbar)
 
-        button_action = QAction(QIcon(r"BrianGUIPractice\myApp\08_Layouts\home.png"),"Your button", self) # <-- QIcon() added to display the icon image designated by the path.  Note r before the directory to get raw string variables
+        button_action = QAction(QIcon(r"BrianGUIPractice\myApp\08_Layouts\home.png"),"&Your button", self) # <-- QIcon() added to display the icon image designated by the path.  Note r before the directory to get raw string variables
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.onMyToolBarButtonClick)
         button_action.setCheckable(True)
@@ -40,7 +40,7 @@ class MainWindow(QMainWindow):
 
         toolbar.addSeparator()
 
-        button_action2 = QAction(QIcon(r"BrianGUIPractice\myApp\08_Layouts\home.png"), "Your button2", self)
+        button_action2 = QAction(QIcon(r"BrianGUIPractice\myApp\08_Layouts\home.png"), "Your &button2", self)
         button_action2.setStatusTip("This is your button2")
         button_action2.triggered.connect(self.onMyToolBarButtonClick)
         button_action2.setCheckable(True)
@@ -50,6 +50,11 @@ class MainWindow(QMainWindow):
         toolbar.addWidget(QCheckBox())
 
         self.setStatusBar(QStatusBar(self))
+
+        menu = self.menuBar()
+
+        file_menu = menu.addMenu("&File")
+        file_menu.addAction(button_action)
 
     def onMyToolBarButtonClick(self, s):
         print("click", s)
