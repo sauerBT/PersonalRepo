91d49a72a967bf46a0beb5aaba5fe8624476c371
SauerBT <Briansauerborn@gmail.com>
2022-06-07 05:54:14 -0400

    Initial commit for section 11 "Windows".  This is the first instance of a window example

---
+++ BrianPersonalCodingRepo/BrianGUIPractice/myApp/08_Layouts/Section11.py
@@ -0,0 +1,54 @@
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import (QApplication, 
+QMainWindow,
+QPushButton,
+QVBoxLayout,
+QWidget,
+QLabel)
+
+# Only needed for access to command line arguments
+import sys
+
+class AnotherWindow(QWidget):
+    """
+    This "window" is a QWidget. If it has no parent, it
+    will appear as a free-floating window.
+    """
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("Another Window")
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        self.button = QPushButton("Push for Window")
+        self.button.clicked.connect(self.show_new_window)
+        self.setMinimumSize(QSize(300,200))
+        self.setCentralWidget(self.button)
+
+    def show_new_window(self, checked):
+        self.w = AnotherWindow()
+        self.w.show()
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([])
+# works too.
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+#Start the event loop
+app.exec() 
+
+# Your application won't reach here until you exit and the event
+# loop has stopped.
+
