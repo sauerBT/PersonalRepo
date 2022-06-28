56171188165ac3d642cd9732d25ae3d3e6ce9962
SauerBT <Briansauerborn@gmail.com>
2022-06-28 08:33:53 -0400

    Commit event based approach to creating a context menu

--- BrianPersonalCodingRepo/BrianGUIPractice/myApp/08_Layouts/Section12.py
+++ BrianPersonalCodingRepo/BrianGUIPractice/myApp/08_Layouts/Section12.py
@@ -2,9 +2,11 @@
 import sys
 
 from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import (QApplication, 
 QMainWindow,
 QLabel,
+QMenu,
 QWidget,
 QTextEdit)
 
@@ -12,51 +14,14 @@ QTextEdit)
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.label = QLabel("Click in this window")
         self.setMinimumSize(QSize(300,200))
-        self.setCentralWidget(self.label)
 
-    def mouseMoveEvent(self, e): # <--------- New Commit
-        self.label.setText("mouseMoveEvent")
-
-    def mousePressEvent(self, e):
-        if e.button() == Qt.MouseButton.LeftButton:
-            # handle the left-button press in here
-            self.label.setText("mousePressEvent LEFT")
-        
-        elif e.button() == Qt.MouseButton.MiddleButton:
-            #handle the middle-button press in here
-            self.label.setText("mousePressEvent MIDDLE")
-
-        elif e.button() == Qt.MouseButton.RightButton:
-            # handle the right-button press in here
-            self.label.setText("mousePressEvent RIGHT")
-
-    def mouseReleaseEvent(self, e):  # <--------- New Commit
-        if e.button() == Qt.MouseButton.LeftButton:
-            # handle the left-button press in here
-            self.label.setText("mouseReleaseEvent LEFT")
-        
-        elif e.button() == Qt.MouseButton.MiddleButton:
-            #handle the middle-button press in here
-            self.label.setText("mouseReleaseEvent MIDDLE")
-
-        elif e.button() == Qt.MouseButton.RightButton:
-            # handle the right-button press in here
-            self.label.setText("mouseReleaseEvent RIGHT")
-
-    def mouseDoubleClickEvent(self, e):  # <--------- New Commit
-        if e.button() == Qt.MouseButton.LeftButton:
-            # handle the left-button press in here
-            self.label.setText("mouseDoubleClickEvent LEFT")
-        
-        elif e.button() == Qt.MouseButton.MiddleButton:
-            #handle the middle-button press in here
-            self.label.setText("mouseDoubleClickEvent MIDDLE")
-
-        elif e.button() == Qt.MouseButton.RightButton:
-            # handle the right-button press in here
-            self.label.setText("mouseDoubleClickEvent RIGHT")
+    def contextMenuEvent(self, e):
+        context = QMenu(self)
+        context.addAction(QAction("test 1", self))
+        context.addAction(QAction("test 2", self))
+        context.addAction(QAction("test 3", self))
+        context.exec(e.globalPos())
 
 
 # You need one (and only one) QApplication instance per application.
