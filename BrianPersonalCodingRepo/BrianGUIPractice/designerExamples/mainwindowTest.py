22cd958aba594470a753b4c82bd8ade358ce58ad
SauerBT <Briansauerborn@gmail.com>
2022-06-30 07:14:53 -0400

    Commint call to Qt Designer window and interact with object signals

--- BrianPersonalCodingRepo/BrianGUIPractice/designerExamples/mainwindowTest.py
+++ BrianPersonalCodingRepo/BrianGUIPractice/designerExamples/mainwindowTest.py
@@ -1,5 +1,4 @@
-from MainWindow import Ui_MainWindow
-
+import random
 import sys
 
 from PyQt6.QtCore import QSize, Qt
@@ -12,10 +11,28 @@ QMenu,
 QWidget,
 QTextEdit)
 
+from MainWindow import Ui_MainWindow
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.show()
+
+        # you can still override from you UI file within your code, 
+        # but if possible, set them in Qt Creator.  See the properties panel
+
+        f = self.label.font() # <---- start interaction with existing object from Ui_MainWindow
+        f.setPointSize(25)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignVCenter)
+        self.label.setFont(f)
+
+        #signals from UI widgets can be connected as normal
+        self.pushButton.pressed.connect(self.update_label) # create signal that interacts with Ui_MainWindow object "pushButton"
+
+    def update_label(self):
+        n = random.randint(1, 6)
+        self.label.setText("%d" % n)
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
@@ -23,8 +40,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 # works too.
 app = QApplication(sys.argv)
 
-window = MainWindow()
-window.show()
+w = MainWindow()
 
 #Start the event loop
 app.exec() 
