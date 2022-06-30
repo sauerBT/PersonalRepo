c15dc16fb7bc54eee0c0ad0554b1924160b76055
SauerBT <Briansauerborn@gmail.com>
2022-06-30 06:59:15 -0400

    Commit converted Qt Ui file to .py and use MainWindowTest to import and display

--- BrianPersonalCodingRepo/BrianGUIPractice/designerExamples/mainwindowTest.py
+++ BrianPersonalCodingRepo/BrianGUIPractice/designerExamples/mainwindowTest.py
@@ -1,9 +1,33 @@
+from MainWindow import Ui_MainWindow
+
 import sys
 
-from PyQt6 import QtWidgets, uic
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import (QApplication, 
+QMainWindow,
+QLabel,
+QPushButton,
+QMenu,
+QWidget,
+QTextEdit)
 
-app = QtWidgets.QApplication(sys.argv)
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
 
-window = uic.loadUi("C:/Users/brian/iCloudDrive/GIT/BrianPersonalCodingRepo/BrianGUIPractice/designerExamples/mainwindowTest.ui")
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([])
+# works too.
+app = QApplication(sys.argv)
+
+window = MainWindow()
 window.show()
-app.exec()
+
+#Start the event loop
+app.exec() 
+
+# Your application won't reach here until you exit and the event
+# loop has stopped.
