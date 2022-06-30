12a73be6e43c614c35819014e4047d2fe6f19c2b
SauerBT <Briansauerborn@gmail.com>
2022-06-30 07:28:11 -0400

    Custom dark palette global application theme

--- BrianPersonalCodingRepo/BrianGUIPractice/designerExamples/mainwindowTest.py
+++ BrianPersonalCodingRepo/BrianGUIPractice/designerExamples/mainwindowTest.py
@@ -13,12 +13,6 @@ QTextEdit)
 
 from MainWindow import Ui_MainWindow
 
-
-palette = QPalette()
-palette.setColor(QPalette.ColorRole.Window, QColor(53,53,53))
-palette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor.yellow)
-palette.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole.WindowText, Qt.GlobalColor.white)
-
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
@@ -45,8 +39,43 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 # If you know you won't use command line arguments QApplication([])
 # works too.
 app = QApplication(sys.argv)
-app.setStyle("Fusion")
-app.setPalette(palette)
+darkPalette = QPalette()
+darkPalette.setColor(QPalette.ColorRole.Window, QColor(53, 53, 53))
+darkPalette.setColor(QPalette.ColorRole.WindowText, Qt.GlobalColor
+.white)
+darkPalette.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole
+.WindowText, QColor(127, 127, 127))
+darkPalette.setColor(QPalette.ColorRole.Base, QColor(42, 42, 42))
+darkPalette.setColor(QPalette.ColorRole.AlternateBase, QColor(66, 66,
+66))
+darkPalette.setColor(QPalette.ColorRole.ToolTipBase, Qt.GlobalColor
+.white)
+darkPalette.setColor(QPalette.ColorRole.ToolTipText, Qt.GlobalColor
+.white)
+darkPalette.setColor(QPalette.ColorRole.Text, Qt.GlobalColor.white)
+darkPalette.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole
+.Text, QColor(127, 127, 127))
+darkPalette.setColor(QPalette.ColorRole.Dark, QColor(35, 35, 35))
+darkPalette.setColor(QPalette.ColorRole.Shadow, QColor(20, 20, 20))
+darkPalette.setColor(QPalette.ColorRole.Button, QColor(53, 53, 53))
+darkPalette.setColor(QPalette.ColorRole.ButtonText, Qt.GlobalColor
+.white)
+191
+darkPalette.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole
+.ButtonText, QColor(127, 127, 127))
+darkPalette.setColor(QPalette.ColorRole.BrightText, Qt.GlobalColor.
+red)
+darkPalette.setColor(QPalette.ColorRole.Link, QColor(42, 130, 218))
+darkPalette.setColor(QPalette.ColorRole.Highlight, QColor(42, 130,
+218))
+darkPalette.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole
+.Highlight, QColor(80, 80, 80))
+darkPalette.setColor(QPalette.ColorRole.HighlightedText, Qt
+.GlobalColor.white)
+darkPalette.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole
+.HighlightedText, QColor(127, 127, 127))
+
+app.setPalette(darkPalette)
 
 w = MainWindow()
 
